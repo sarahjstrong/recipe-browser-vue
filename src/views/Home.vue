ac200811e1913b59322bbf0c1dd96361591ecd99
Sarah Strong <148368227+sarahjstrong@users.noreply.github.com>
2024-01-26 16:34:27 -0500

    Add files via upload

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="flex flex-col p-8">
+        <div>
+            <input type="text" class="rounded border-2 border-gray-200 w-full" placeholder="Search for meals..." />
+        </div>
+
+        <div class="flex gap-1">
+            <router-link to="/" v-for="letter of letters">
+                {{ letter }}
+            </router-link>
+        </div>
+
+    </div>
+</template>
+
+<script setup>
+    import { computed } from 'vue';
+import store from '../store';
+    const meals = computed(() => store.state.meals)
+    const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split("");
+</script>
