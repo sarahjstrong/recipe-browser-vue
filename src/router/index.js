887419c147a86e2ee25ca251df3c77c0598ee044
Sarah Strong <artbysarahjessicastrong@gmail.com>
2024-01-27 13:17:16 -0500

    new routes and connection to API

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,29 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import MealsByIngredient from '../views/MealsByIngredient.vue'
+import MealsByLetter from '../views/MealsByLetter.vue'
+import MealsByName from '../views/MealsByName.vue'
 
 const routes = [
     {
         path: '/',
         name: 'home',
         component: Home,
+    },
+    {
+        path: '/by-letter/:letter?',
+        name: 'byLetter',
+        component: MealsByLetter,
+    },
+    {
+        path: '/by-name/:name?',
+        name: 'byName',
+        component: MealsByName,
+    },
+    {
+        path: '/by-ingredient/:ingredient?',
+        name: 'byIngredient',
+        component: MealsByIngredient,
     }
 ]
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,8 +4,8 @@
             <input type="text" class="rounded border-2 border-gray-200 w-full" placeholder="Search for meals..." />
         </div>
 
-        <div class="flex gap-1">
-            <router-link to="/" v-for="letter of letters">
+        <div class="flex justify-center gap-2 mt-2">
+            <router-link :to="{name: 'byLetter', params: {letter}}" v-for="letter of letters" :key="letter">
                 {{ letter }}
             </router-link>
         </div>
@@ -14,8 +14,16 @@
 </template>
 
 <script setup>
-    import { computed } from 'vue';
+    import { computed, onMounted, ref } from 'vue';
 import store from '../store';
-    const meals = computed(() => store.state.meals)
+import axiosClient from '../axiosClient.js'
+    
     const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split("");
+    const ingredients = ref([])
+
+    onMounted(async () => {
+        const response = await axiosClient.get('/list.php?i=list');
+        console.log(response.data);
+        ingredients.value = response.data;
+    })
 </script>
